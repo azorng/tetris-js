ff7f9179ebc7bd312596d91cd51f0a6629418007
Azor <i@azor.net>
2022-01-01 18:26:05 +0100

    move figure models out of class

--- figure.js
+++ figure.js
@@ -10,7 +10,7 @@ class Figure {
     shape
 
     constructor(game) {
-        const f = this.figures[Util.rng(0, this.figures.length - 1)]
+        const f = figureModels[Util.rng(0, figureModels.length - 1)]
         this.game = game
         this.shape = f.shape
         this.color = f.color
@@ -51,63 +51,63 @@ class Figure {
             this.position.y++
         })
     }
-
-    figures = [
-        {
-            shape: [
-                [0, 0, 0, 0],
-                [1, 1, 1, 1],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0]
-            ],
-            color: '#03A9F4'
-        },
-        {
-            shape: [
-                [1, 1, 1],
-                [1, 0, 0],
-                [0, 0, 0]
-            ],
-            color: '#FFC107'
-        },
-        {
-            shape: [
-                [1, 1, 1],
-                [0, 1, 0],
-                [0, 0, 0]
-            ],
-            color: '#673AB7'
-        },
-        {
-            shape: [
-                [1, 1, 1],
-                [0, 0, 1],
-                [0, 0, 0]
-            ],
-            color: '#3F51B5'
-        },
-        {
-            shape: [
-                [1, 1],
-                [1, 1]
-            ],
-            color: '#FFEB3B'
-        },
-        {
-            shape: [
-                [0, 1, 1],
-                [1, 1, 0],
-                [0, 0, 0]
-            ],
-            color: '#4CAF50'
-        },
-        {
-            shape: [
-                [1, 1, 0],
-                [0, 1, 1],
-                [0, 0, 0]
-            ],
-            color: '#673AB7'
-        }
-    ]
 }
+
+const figureModels = [
+    {
+        shape: [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        color: '#03A9F4'
+    },
+    {
+        shape: [
+            [1, 1, 1],
+            [1, 0, 0],
+            [0, 0, 0]
+        ],
+        color: '#FFC107'
+    },
+    {
+        shape: [
+            [1, 1, 1],
+            [0, 1, 0],
+            [0, 0, 0]
+        ],
+        color: '#673AB7'
+    },
+    {
+        shape: [
+            [1, 1, 1],
+            [0, 0, 1],
+            [0, 0, 0]
+        ],
+        color: '#3F51B5'
+    },
+    {
+        shape: [
+            [1, 1],
+            [1, 1]
+        ],
+        color: '#FFEB3B'
+    },
+    {
+        shape: [
+            [0, 1, 1],
+            [1, 1, 0],
+            [0, 0, 0]
+        ],
+        color: '#4CAF50'
+    },
+    {
+        shape: [
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0]
+        ],
+        color: '#673AB7'
+    }
+]
